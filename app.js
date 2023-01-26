f7964977e459107b303b010506c4df88ba24d162
amrhany20 <amrhany1214@gmail.com>
2023-01-26 22:14:01 +0200

    adding level 3 of security - hashing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 const encrypt = require("mongoose-encryption");
 const app = express();
+const md5 =require("md5");
 
 app.set('view engine', 'ejs');
 
@@ -20,7 +21,7 @@ const userSchema = new mongoose.Schema ({
     email:String,
     password:String
 });
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 const User = new mongoose.model("User",userSchema);
 
 app.get("/",function (req,res) {
@@ -35,7 +36,7 @@ app.get("/register",function (req,res) {
 app.post("/register",function (req,res) {
     const newUser = new User({
         email:req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function (err) {
         if (err) {
@@ -47,7 +48,7 @@ app.post("/register",function (req,res) {
 });
 app.post("/login",function (req,res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email:username},function (err,foundUser) {
         if (err) {
             console.log(err);
