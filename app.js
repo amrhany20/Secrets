a481f11dc824d95461575d3da1138c50b5550d1a
amrhany20 <amrhany1214@gmail.com>
2023-01-26 22:42:02 +0200

    Adding 4th level of security - hashing and salting

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 const encrypt = require("mongoose-encryption");
 const app = express();
-const md5 =require("md5");
-
+//const md5 =require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds =10;
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -34,31 +35,47 @@ app.get("/register",function (req,res) {
     res.render("register")
 });
 app.post("/register",function (req,res) {
-    const newUser = new User({
-        email:req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email:req.body.username,
+            password:hash
+           // password: md5(req.body.password)
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+    
     });
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
+    
 });
 app.post("/login",function (req,res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password=req.body.password;
+
+    //const password = md5(req.body.password);
     User.findOne({email:username},function (err,foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if(foundUser.password===password){
-                    res.render("secrets")
-                }else{
-                    console.log("wrong password");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result) {
+                        res.render("secrets");
+                    } else {
+                        console.log("wrong password");
+                    }
+                });
+                
+                // if(foundUser.password===password){
+                //     res.render("secrets")
+                // }else{
+                //     console.log("wrong password");
+                // }
             } else {
                 console.log("no such a user registered");
             }
